7494afaa96ec7f4bf0b14168bf6e722fa801f264
Skyler Hill <10922017@uvu.edu>
2021-09-08 17:11:06 -0600

    Finished Prototype Tank Sim

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10.5f;
     public float turnSpeed;
+    public float hInput;
+    public float vInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        //Move the Tank
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * hInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
     }
 }
---
+++ Prototype 1/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject tank;
+    private Vector3 offset = new Vector3(0, 50, -100);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = tank.transform.position + offset;
+    }
+}
