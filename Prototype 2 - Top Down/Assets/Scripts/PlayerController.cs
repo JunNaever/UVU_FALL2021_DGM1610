f24e9b666000de26e387ced88d5d24888ee74042
JunNaever <10922017@uvu.edu>
2021-10-12 18:52:17 -0600

    Lab2 Proto started

--- Prototype 2 - Top Down/Assets/Scripts/PlayerController.cs
+++ Prototype 2 - Top Down/Assets/Scripts/PlayerController.cs
@@ -4,38 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
-    public float turnSpeed  = 100f;
-    public float hInput;
-    public float vInput;
-    public float xRange = 9.67f;
-    public float yRange = 4.95f;
-    public GameObject projectile;
-    public Transform launcher;
-    public Vector3 offset = new Vector3(0, 1, 0);
+  public float speed = 20.0f;
+  public float shockSpeed;
+  public float turnSpeed = 100f;
+  public float shockTurnSpeed;
+  public float hInput;
+  public float vInput;
+  public float xRange = 9.67f;
+  public float yRange = 4.95f;
+  public GameObject projectile;
+  public Transform launcher;
+  public Vector3 offset = new Vector3(0, 1, 0);
+  float saveSpeed;
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    hInput = Input.GetAxis("Horizontal");
+    vInput = Input.GetAxis("Vertical");
+
+    //Setup Player Movement and Rotation
+    transform.Rotate(Vector3.back * turnSpeed * hInput * Time.deltaTime);
+    transform.Translate(Vector3.up * speed * vInput * Time.deltaTime);
+
+    //Prevent player from leaving the screen.
+    if (transform.position.x < -xRange)
+      transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    if (transform.position.x > xRange)
+      transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    if (transform.position.y < -yRange)
+      transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+    if (transform.position.y > yRange)
+      transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+
+    //Shoot Missile
+    if (Input.GetKeyDown(KeyCode.Space))
+      Instantiate(projectile, launcher.transform.position, launcher.transform.rotation);
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.CompareTag("Electric"))
     {
-        hInput = Input.GetAxis("Horizontal");
-        vInput = Input.GetAxis("Vertical");
-
-        //Setup Player Movement and Rotation
-        transform.Rotate(Vector3.back * turnSpeed * hInput * Time.deltaTime);
-        transform.Translate(Vector3.up * speed * vInput * Time.deltaTime);
-
-        //Prevent player from leaving the screen.
-        if (transform.position.x < -xRange)
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        if (transform.position.x > xRange)
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        if (transform.position.y < -yRange)
-            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
-        if (transform.position.y > yRange)
-            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
-        
-        //Shoot Missile
-        if (Input.GetKeyDown(KeyCode.Space))
-            Instantiate(projectile, launcher.transform.position, launcher.transform.rotation);
+      saveSpeed = speed;
+      speed = shockSpeed;
     }
+  }
+  void OnTriggerExit2D(Collider2D other)
+  {
+    if (other.CompareTag("Electric"))
+    {
+      speed = saveSpeed;
+    }
+  }
 }
