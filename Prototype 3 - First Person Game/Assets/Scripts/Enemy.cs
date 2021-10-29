9d1c42307b420da25812f2d40b55da086e856a05
JunNaever <10922017@uvu.edu>
2021-10-28 19:11:52 -0600

    Added enemy script with base enemy mechanics

--- Prototype 3 - First Person Game/Assets/Scripts/Enemy.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/Enemy.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  //Enemy Stats
+  public int curHp, maxHp, scoreToGive;
+  //Movement
+  public float moveSpeed, attackRange, yPathOffset;
+  private List<Vector3> path;
+  //Target to follow
+  private GameObject target;
+  //Enemy Weapon
+  private Weapon weapon;
 
-    // Update is called once per frame
-    void Update()
+  void Start()
+  {
+    weapon = GetComponent<Weapon>();
+    target = FindObjectOfType<PlayerController>().gameObject;
+    curHp = maxHp;
+    InvokeRepeating("UpdatePath", 0.0f, 0.5f);
+  }
+
+  void Update()
+  {
+    //Look at the target
+    Vector3 dir = (target.transform.position - transform.position).normalized;
+    float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+    transform.eulerAngles = Vector3.up * angle;
+
+    //Calculate the distance between the enemy and the target
+    float dist = Vector3.Distance(transform.position, target.transform.position);
+    //If within attackRange, shoot at target
+    if (dist <= attackRange)
     {
-        
+      if (weapon.CanShoot())
+        weapon.Shoot();
     }
+    //If the enemy is too far away, chase the target
+    else
+    {
+      ChaseTarget();
+    }
+  }
+
+  void UpdatePath()
+  {
+    //Calculate a path to the target
+    NavMeshPath navMeshPath = new NavMeshPath();
+    NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
+
+    path = navMeshPath.corners.ToList();
+  }
+
+  void ChaseTarget()
+  {
+    if (path.Count == 0)
+      return;
+    //Move towards the closest path
+    transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0, yPathOffset, 0), moveSpeed * Time.deltaTime);
+
+    if (transform.position == path[0] + new Vector3(0, yPathOffset, 0))
+      path.RemoveAt(0);
+  }
+
 }
